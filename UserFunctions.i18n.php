e475f8a9dff08c7503a6da59d65289a0adaf895e
Raimond Spekking <raymond@users.mediawiki.org>
2011-12-19 20:15:36 +0000

    Localisation updates for core and extension messages from translatewiki.net

--- UserFunctions.i18n.php
+++ UserFunctions.i18n.php
@@ -12,3 +12,38 @@ $messages['en'] = array(
 	'userfunctions-desc' => 'Enhance parser with user functions',
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'userfunctions-desc' => 'Ergänzt Parserfunktionen, die benutzerbezogene Funktionen auf Wikiseiten ermöglichen',
+);
+
+/** Lower Sorbian (Dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'userfunctions-desc' => 'Parser wó wužywarske funkcije rozšyriś',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'userfunctions-desc' => 'Mellora o analizador con funcións de usuario',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'userfunctions-desc' => 'Parser wo wužiwarske funkcije rozšěrić',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'userfunctions-desc' => 'Breidt de parser uit met gebruikersfuncties',
+);
+
